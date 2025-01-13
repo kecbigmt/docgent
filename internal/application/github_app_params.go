e64666842729430254fbffdfa27a341154b5f9ca
kecy <keishiro.oyama@kecy.me>
2025-01-13 20:55:29 +0900

    refactor: Move GitHubAppParams

---
+++ internal/application/github_app_params.go
@@ -0,0 +1,8 @@
+package application
+
+type GitHubAppParams struct {
+	Owner          string
+	Repo           string
+	DefaultBranch  string
+	InstallationID int64
+}
--- internal/application/slack_event_route.go
+++ internal/application/slack_event_route.go
@@ -2,13 +2,6 @@ package application
 
 import "github.com/slack-go/slack/slackevents"
 
-type GitHubAppParams struct {
-	Owner          string
-	Repo           string
-	DefaultBranch  string
-	InstallationID int64
-}
-
 type SlackEventRoute interface {
 	ConsumeEvent(event slackevents.EventsAPIInnerEvent, githubAppParams GitHubAppParams)
 	EventType() string
