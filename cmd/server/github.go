d0c1eaf800762418cbb670008e6410a0b406b289
kecy <keishiro.oyama@kecy.me>
2025-01-21 08:44:32 +0900

    chore(server): Correct log message

--- cmd/server/github.go
+++ cmd/server/github.go
@@ -12,7 +12,7 @@ import (
 func NewGitHubAPI() *github.API {
 	appIDStr := os.Getenv("GITHUB_APP_ID")
 	if appIDStr == "" {
-		log.Fatal("GITHUB_APP_PRIVATE_KEY is not set")
+		log.Fatal("GITHUB_APP_ID is not set")
 	}
 	appID, err := strconv.ParseInt(appIDStr, 10, 64)
 	if err != nil {
