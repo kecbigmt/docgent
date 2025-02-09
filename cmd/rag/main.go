24b448dda2ef0d22433ae2bf939603f1b95335d4
kecy <keishiro.oyama@kecy.me>
2025-02-09 17:02:18 +0900

    feat(rag): enhance CLI structure to support file upload and deletion commands

--- cmd/rag/main.go
+++ cmd/rag/main.go
@@ -15,26 +15,27 @@ import (
 )
 
 var CLI struct {
-	File        string `arg:"" help:"Path to the file to upload" type:"path"`
-	CorpusID    string `required:"" help:"ID of the RAG corpus"`
-	Description string `help:"Description of the file"`
-	ChunkSize   int    `help:"Size of each chunk" default:"1000"`
-	Overlap     int    `help:"Overlap size between chunks" default:"100"`
-	ProjectID   string `required:"" help:"Google Cloud Project ID"`
-	Location    string `help:"Google Cloud location" default:"us-central1"`
+	Upload struct {
+		File        string `arg:"" help:"Path to the file to upload" type:"path"`
+		CorpusID    string `required:"" help:"ID of the RAG corpus"`
+		Description string `help:"Description of the file"`
+		ChunkSize   int    `help:"Size of each chunk" default:"1000"`
+		Overlap     int    `help:"Overlap size between chunks" default:"100"`
+	} `cmd:"" help:"Upload a file to the RAG corpus"`
+
+	Delete struct {
+		CorpusID string `required:"" help:"ID of the RAG corpus"`
+		FileID   string `required:"" help:"ID of the RAG file to delete"`
+	} `cmd:"" help:"Delete a file from the RAG corpus"`
+
+	ProjectID string `required:"" help:"Google Cloud Project ID"`
+	Location  string `help:"Google Cloud location" default:"us-central1"`
 }
 
 func main() {
 	ctx := context.Background()
 
-	kong.Parse(&CLI)
-
-	file, err := os.Open(CLI.File)
-	if err != nil {
-		fmt.Printf("Failed to open file: %v\n", err)
-		os.Exit(1)
-	}
-	defer file.Close()
+	kongCtx := kong.Parse(&CLI)
 
 	credentials, err := google.FindDefaultCredentials(ctx)
 	if err != nil {
@@ -44,33 +45,69 @@ func main() {
 
 	client := lib.NewClientWithCredentials(credentials, CLI.ProjectID, CLI.Location)
 
-	corpusID, err := strconv.ParseInt(CLI.CorpusID, 10, 64)
-	if err != nil {
-		fmt.Printf("Invalid corpus ID format: %v\n", err)
-		os.Exit(1)
+	switch kongCtx.Command() {
+	case "upload":
+		file, err := os.Open(CLI.Upload.File)
+		if err != nil {
+			fmt.Printf("Failed to open file: %v\n", err)
+			os.Exit(1)
+		}
+		defer file.Close()
+
+		corpusID, err := strconv.ParseInt(CLI.Upload.CorpusID, 10, 64)
+		if err != nil {
+			fmt.Printf("Invalid corpus ID format: %v\n", err)
+			os.Exit(1)
+		}
+
+		fileName := filepath.Base(CLI.Upload.File)
+
+		createdFile, err := client.UploadFile(ctx, corpusID, file, fileName, func(o *lib.UploadFileOptions) {
+			if CLI.Upload.Description != "" {
+				o.Description = CLI.Upload.Description
+			}
+			o.ChunkingConfig = lib.ChunkingConfig{
+				ChunkSize:    CLI.Upload.ChunkSize,
+				ChunkOverlap: CLI.Upload.Overlap,
+			}
+		})
+
+		if err != nil {
+			var httpErr *lib.HTTPError
+			if errors.As(err, &httpErr) {
+				fmt.Printf("Failed to upload file: %s %s\n", httpErr.Status, httpErr.RawBody)
+			} else {
+				fmt.Printf("Failed to upload file: %v\n", err)
+			}
+			os.Exit(1)
+		}
+
+		fmt.Printf("Successfully uploaded file '%s'\n", createdFile.Name)
+
+	case "delete":
+		corpusID, err := strconv.ParseInt(CLI.Delete.CorpusID, 10, 64)
+		if err != nil {
+			fmt.Printf("Invalid corpus ID format: %v\n", err)
+			os.Exit(1)
+		}
+
+		fileID, err := strconv.ParseInt(CLI.Delete.FileID, 10, 64)
+		if err != nil {
+			fmt.Printf("Invalid file ID format: %v\n", err)
+			os.Exit(1)
+		}
+
+		err = client.DeleteFile(ctx, corpusID, fileID)
+		if err != nil {
+			var httpErr *lib.HTTPError
+			if errors.As(err, &httpErr) {
+				fmt.Printf("Failed to delete file: %s %s\n", httpErr.Status, httpErr.RawBody)
+			} else {
+				fmt.Printf("Failed to delete file: %v\n", err)
+			}
+			os.Exit(1)
+		}
+
+		fmt.Printf("Successfully deleted file from corpus %d\n", corpusID)
 	}
-
-	fileName := filepath.Base(CLI.File)
-
-	createdFile, err := client.UploadFile(ctx, corpusID, file, fileName, func(o *lib.UploadFileOptions) {
-		if CLI.Description != "" {
-			o.Description = CLI.Description
-		}
-		o.ChunkingConfig = lib.ChunkingConfig{
-			ChunkSize:    CLI.ChunkSize,
-			ChunkOverlap: CLI.Overlap,
-		}
-	})
-
-	if err != nil {
-		var httpErr *lib.HTTPError
-		if errors.As(err, &httpErr) {
-			fmt.Printf("Failed to upload file: %s %s\n", httpErr.Status, httpErr.RawBody)
-		} else {
-			fmt.Printf("Failed to upload file: %v\n", err)
-		}
-		os.Exit(1)
-	}
-
-	fmt.Printf("Successfully uploaded file '%s'\n", createdFile.Name)
 }
